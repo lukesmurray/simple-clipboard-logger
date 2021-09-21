147e9fc9d04af40512385eca703b95860421bb2c
Luke Murray <lukepigeonmail@gmail.com>
2021-09-21 14:05:20 -0400

    switch to official rollup plugins and use browesrslist and babel

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,9 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import camelCase from "lodash.camelcase";
-import commonjs from "rollup-plugin-commonjs";
-import json from "rollup-plugin-json";
-import resolve from "rollup-plugin-node-resolve";
 import sourceMaps from "rollup-plugin-sourcemaps";
-import typescript from "rollup-plugin-typescript2";
+import ts from "rollup-plugin-ts";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require("./package.json");
@@ -15,6 +15,12 @@ export default {
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: "umd", sourcemap: true },
     { file: pkg.module, format: "es", sourcemap: true },
+    {
+      file: pkg.browser,
+      name: camelCase(libraryName),
+      format: "iife",
+      sourcemap: true,
+    },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
@@ -25,15 +31,22 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
+    ts({
+      transpiler: "babel",
+    }),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+    // https://github.com/rollup/plugins/tree/master/packages/node-resolve
+    nodeResolve(),
+
+    // because most packages on npm are commonjs we use this plugin so rollup
+    // can bundle commonjs modules
+    commonjs(),
 
     // Resolve source maps to the original source
     sourceMaps(),
+
+    // minify the output
+    // terser(),
   ],
 };
